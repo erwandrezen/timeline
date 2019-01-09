5399846e289a76df7ccaa44b1a09a24c14ed0678
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-09 16:43:54 +0100

    Probleme de parcour

--- html/jeu d'essai/php/Algorithme parcours en largeur.php
+++ html/jeu d'essai/php/Algorithme parcours en largeur.php
@@ -22,16 +22,63 @@
     }
 
     // Recupere les sommets voisin
-    function sommet_voisin(request_response, sommet){
-        var sommet_nom = [];
-        	request_response[sommet].forEach(function(e){
-            	// Enregistrer sommet - dessiner (plus tard)
-        		sommet_nom.push(e);
-        	});
-        console.log(sommet_nom);
-        return sommet_nom;
+    function sommet_voisin(sommet){
+    	var table = sommet;
+    	var keys = Object.keys(table);
+    	var values = Object.values(table);
+		var marquer = ['gf'];
+		var i = 0;
+		marquer.forEach(function(e){
+			marquer[i] = e
+			console.log(marquer[i]);
+			i++;
+			//marquer.push(keys);
+		});
+		
+    	console.log	(marquer);
     }
+/*
+    explorer (graph, sommet)
+    sauvegarder sommet.keys
+    dessiner rectangle
+    s = keys
+      push s
+      for each dernier element de s
+      	concate sous-elem
+      
+      explorer(concate)
+*/
+var marquer = [];
+var i = 0;
+	function explorer(sommet){
+		var table = sommet;
+		var keys = Object.keys(table);
+		var values = Object.values(table);
+		
+		marquer.push(values);
+		console.log(values);
+		var lastElement = marquer.slice(-1);
+		//console.log(lastElement);
+		var newArray = [];
 
+		if (lastElement.length >= 1){
+			//console.log("ok");
+					lastElement.forEach(function(v,k,e){
+						console.log(v[0]);
+						newArray = newArray.concat(v[0]);
+
+
+						
+						});
+
+		}
+		if (i < 10){
+			i++;
+			//console.log(newArray);
+			explorer(newArray);
+		}
+		
+    }
     // lancement des fonctions pour ouvrir le json,
     // recuperer les sommets
     function get_tree(graph, sommet) {
@@ -39,7 +86,7 @@
     	var file;
         request.onload = function() {        
         	  file = request.response;
-        	  sommet_voisin(file,sommet);
+        	  explorer(file[sommet]);
         	}
     	} 
 
