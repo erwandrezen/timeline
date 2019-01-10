092c997cc40b35339b1a90c27ec9e7125c815631
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-10 16:43:38 +0100

    sauvegarde

--- html/jeu d'essai/js/tree_profondeur.js
+++ html/jeu d'essai/js/tree_profondeur.js
@@ -1,39 +1,56 @@
+
+
 function branche(sommet){
+
     	marquer.push(sommet);
-    	var table = sommet;
-    	var keys = Object.keys(table);
-    	var values = Object.values(table);
-
-
+    	var keys = Object.keys(sommet);
+    	var values = Object.values(sommet);
+    	var lesValeurs = sommet.values;
+    	var unBool = false;
 
 		// Parcourir pour chaque valeurs
-    	values.forEach(function(e){
-    		//console.log(sommet.length);
+    	keys.forEach(function(e){
+   
         	// Si c'est un objet (sous-branche)
-    		if (typeof e == 'object'){
+    		if (typeof sommet[e] == 'object'){
+    			
     			
         		
     			
-				console.log(e);
-        		// Parcourir la sous-branche
-    			// SI ce n'es pas un noeud sans propieter on dessine
-				if (sommet.length){
-					// dessiner un rectangle (plus tard)
-					rectangle(element,x,y,w,h);
-	    			text(element,e['uid'],x,y);
-	    			y += h+2;
-	    			//console.log(sommet.length);
-				} else {
-					// Sinon on change de niveau
-					console.log("niveau suivant");
-					x += w + 2;
-				}
-        		branche(e);
-        		
-    		} else {
-    			//console.log("not object");
-    		}
 
+    			
+   
+    				x += w + 2;
+  
+        		branche(sommet[e]);
+        		
+    		} else if (sommet['uid'] == sommet[e]) {
+    			// Sinon  Ce n'es pas un object
+    		
+    				
+    				// Parcourir la sous-branche
+    				// SI ce n'es pas un noeud sans propieter on dessine
+
+    					//console.log(e);
+    					// dessiner un rectangle (plus tard)
+
+    					rectangle(element,x,y,w,h);
+    					
+    	    			text(element,sommet['uid'],x,y);
+    	    			x = w + 2;
+    	    			y += h+2;
+    	    			console.log(sommet.length);
+    	
+    	
+    				
+    			
+    			
+    			
+    			
+    		} else {
+    		}
+    		
+    		//x = x - w - 2;
 
     	});
 
