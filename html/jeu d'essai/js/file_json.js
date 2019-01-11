52b78e06daa4b55e7b8f8db652e1b45e9aa18cbf
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-11 11:27:41 +0100

    Creation fonction "write_profondeur_json"
    Permet d'écrire la profondeur d'un seul sommet

--- html/jeu d'essai/js/file_json.js
+++ html/jeu d'essai/js/file_json.js
@@ -7,3 +7,24 @@
         request.send();
         return request;
     }
+    
+    
+    function write_profondeur_json(file,sommet,H,L){
+    	
+    	//Ouvrire le fichier
+    	var sommet;
+    	var open = open_json(file);
+    	//Chargement du fichier
+    	open.onload = function(){
+    		// Recuperation des donnees
+    		var profondeur = {hauteur : H, largeur : L};
+    		var response = open.response;
+    		sommet = response[sommet];
+
+
+
+        	sommet["profondeur"] = profondeur;
+        	console.log(sommet);
+    	}
+    	
+    }
