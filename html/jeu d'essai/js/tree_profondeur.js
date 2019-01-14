f03496b3a5628b5eaf5057934a4191578f7088b7
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-14 16:43:03 +0100

    Sauvegarde
    Des erreurs au niveau du parametre profondeur (longueur)

--- html/jeu d'essai/js/tree_profondeur.js
+++ html/jeu d'essai/js/tree_profondeur.js
@@ -1,60 +1,75 @@
+marquer = [];
+var i = 0;
+function branche_profondeur(sommet){
+		
 
 
-function branche(sommet){
-
-    	marquer.push(sommet);
+    	var newArray = [];
+    	
     	var keys = Object.keys(sommet);
+
     	var values = Object.values(sommet);
     	var lesValeurs = sommet.values;
     	var unBool = false;
+    	var essai;
+		var profondeur = {}; // niveau de profondeur pour chaque des sommets
 
 		// Parcourir pour chaque valeurs
     	keys.forEach(function(e){
-   
-        	// Si c'est un objet (sous-branche)
-    		if (typeof sommet[e] == 'object'){
-    			
-    			
-        		
-    			
-
-    			
-   
-    				x += w + 2;
-  
-        		branche(sommet[e]);
-        		
-    		} else if (sommet['uid'] == sommet[e]) {
-    			// Sinon  Ce n'es pas un object
     		
-    				
-    				// Parcourir la sous-branche
-    				// SI ce n'es pas un noeud sans propieter on dessine
+    		
+        	// Si c'est un objet (sous-branche)
+    		//console.log(e);
+    		if (sommet[e].constructor.name == 'Object'
+    			 && e != "profondeur"){
+    			//console.log("branche", parseInt(e) + 1);
+    			
+    			//console.log(profondeur);
+    			//console.log(sommet[e]);
 
-    					//console.log(e);
-    					// dessiner un rectangle (plus tard)
 
-    					rectangle(element,x,y,w,h);
-    					
-    	    			text(element,sommet['uid'],x,y);
-    	    			x = w + 2;
-    	    			y += h+2;
-    	    			console.log(sommet.length);
-    	
-    	
-    				
+    			
+    			// On parcourire l'objet
+    			console.log(e, parseInt(e)+1);
+    			profondeur['largeur'] = parseInt(e) ;
+    			console.log("??: ", sommet[e]);
+    			sommet[e]['profondeur'] = profondeur;
     			
     			
+    			marquer.push(sommet[e]);
+    			branche_profondeur(sommet[e]);
+
+
+    		} 
+    		
+
+    		
+    		if (sommet[e].constructor.name == 'Array'){
+    			
+
+    			// On ajoute le tableau à la liste des tableaux à parcourir
+    			sommet[e].forEach(function(e){
+    				newArray.push(e);
+    			});
+    			
+    			//branche_profondeur(sommet[e]);
     			
     			
     		} else {
     		}
     		
+    		
+    		
     		//x = x - w - 2;
 
     	});
 
-    
+    	if (newArray.length > 0){
+    		//console.log(newArray);
+    		branche_profondeur(newArray);
+		} else {
+			console.log(marquer);
+		}
     }
 /*
     function get_tree(graph, sommet) {
