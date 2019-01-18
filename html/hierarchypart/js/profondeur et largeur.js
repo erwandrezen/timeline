95e147c5e74daacf1ebe443aa4e84f03307e8ae4
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-18 16:04:30 +0100

    Mise en place d'algorithme

---
+++ html/hierarchypart/js/profondeur et largeur.js
@@ -0,0 +1,26 @@
+
+function parcourire_arbre(){
+	
+	/* Nombre de feuilles du noeud N
+	 * N = Somme de feuilles de chaqun de ses noeuds
+	 * 
+	 * */
+	
+	/* S'il n'y as pas d'enfant (impossible de parcourir)
+	 * C'est une feuille
+	 * => Marquer la feuille
+	 */
+	
+	
+	// Si c'est un objet
+		// Enregistrer niveau
+		// Enregistrer hauteur
+		// Ne pas enregistrer ses tableaux
+		// Enregistrer ses tableaux dans une autre variable
+		// Enregistrer cet objet selon sa position et sa hauteur
+
+	
+	
+	// Si c'est un tableau
+		//Pour chaque element du tableaux
+}
