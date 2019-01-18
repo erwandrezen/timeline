ef7f72e8351caabf6cd5ca774e35ceb62562f140
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-18 16:41:09 +0100

    Algorithme

--- html/hierarchypart/js/profondeur et largeur.js
+++ html/hierarchypart/js/profondeur et largeur.js
@@ -1,26 +1,51 @@
 
+
 function parcourire_arbre(){
 	
 	/* Nombre de feuilles du noeud N
 	 * N = Somme de feuilles de chaqun de ses noeuds
-	 * 
 	 * */
 	
-	/* S'il n'y as pas d'enfant (impossible de parcourir)
-	 * C'est une feuille
-	 * => Marquer la feuille
-	 */
+	// ROOT peut être un tableau ou un objet
 	
+	
+	//Exemple de tableau: [Objet,tableau,tableau,Objet]
+	
+	// Si c'est un tableau
+		//Pour chaque element du tableaux
+			//relancer la fonction
+	
+	
+	
+	//Exemple d'objet: {a:'a', b:'b', tableau}
 	
 	// Si c'est un objet
 		// Enregistrer niveau
 		// Enregistrer hauteur
 		// Ne pas enregistrer ses tableaux
-		// Enregistrer ses tableaux dans une autre variable
-		// Enregistrer cet objet selon sa position et sa hauteur
+		
+		// Enregistrer cet objet selon sa position et sa hauteur en fonction d'un tableau final
+	
+					
+					/* S'il n'y as pas d'enfant (impossible de parcourir)
+					 * C'est une feuille
+					 * => Marquer la feuille
+					 * 
+					 * 
+					 * Sinon
+					 * 
+					 * 
+					  Enregistrer ses tableaux dans une autre variable
+						 * Ses tableaux sont applatie
+						 * Exemple: {objet,tableau1,tableau2}
+						 * tableau1 = [sous_element1,sous_element2]
+						 * tableau2 = [sous_element1]
+						 * (ne pas faire) tableau != tableau1 + tableau2
+						 * (mais faire ca)tableau == [sous_element1,sous_element2]
+					*/
+					 
 
 	
 	
-	// Si c'est un tableau
-		//Pour chaque element du tableaux
+	
 }
