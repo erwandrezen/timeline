50faebaaf4b01aa3d69c7e0ae5eccc67d570136e
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-21 15:49:03 +0100

    Parcour en 1 fois
    Ecriture de
    
    - profondeur
    - largeur
    - 1 fin de noeud = 1 feuille

--- html/hierarchypart/js/profondeur et largeur.js
+++ html/hierarchypart/js/profondeur et largeur.js
@@ -1,49 +1,118 @@
 
+/* parcourire_arbre permet de parcourir un arbre en inscrivant la profondeur et ses feuilles du noeud n
+ * 
+ * 
+ * 
+ * le parametre:
+ * 
+ * - sommet renseigne le noeud racine
+ * - profondeur permet la sauvegarder de la profondeur du noeud n
+ * il n'es pas utile de le renseigner lors du lancement de la fonction
+ * il n'es pas nécessaire de le prendre en compte
+ * sert seulement de variable à cette fonction recursive
+ * */
 
-function parcourire_arbre(){
+var essai_tmp = [];
+function parcourire_arbre(sommet,profondeur = {profondeur:0, hauteur:0,feuilles_noeud:1},parent_noeud = []){
 	
-	/* Nombre de feuilles du noeud N
-	 * N = Somme de feuilles de chaqun de ses noeuds
-	 * */
-	
-	// ROOT peut être un tableau ou un objet
-	
-	
-	//Exemple de tableau: [Objet,tableau,tableau,Objet]
-	
-	// Si c'est un tableau
-		//Pour chaque element du tableaux
-			//relancer la fonction
-	
-	
-	
-	//Exemple d'objet: {a:'a', b:'b', tableau}
-	
-	// Si c'est un objet
-		// Enregistrer niveau
-		// Enregistrer hauteur
-		// Ne pas enregistrer ses tableaux
+	// Si l'element est une liste (contient des sous noeud)
+	if (sommet.constructor.name == "Array"){
+
+
+		//console.log(sommet);
 		
-		// Enregistrer cet objet selon sa position et sa hauteur en fonction d'un tableau final
+		// si l'element contient des donnees
+		if (sommet.length > 0){
+			
+			// on ajoute un niveau de profondeur
+			profondeur.profondeur += 1;
+			
+			/* pour chaque element 
+			 * e = valeur d'une element du tableau
+			 * i = l'indice de cet element du tableau
+			*/
+			let ii = 1;
+			sommet.forEach(function(e,i){
+				//console.log(e);
+				// profondeur.hauteur = index du tableau qu'on parcour + 1
+				
+				/*  /!\ l'element peut etre soit un tableau soit un objet
+				 * Mais la profondeur n'es pas sauvegarder à cette endroit dans le tableau
+				 * la hauteur est egal a l'indice d'un element
+				*/
+				profondeur.hauteur = i+1;
+				ii = profondeur.hauteur;
+				// on parcoure l'element e en sauvegardant la profondeur
+				parcourire_arbre(e,profondeur,parent_noeud);
+				
+			});
+			
+			//console.log("retour en haut", sommet);
+			profondeur.profondeur -= 1;
+		}
+		
+		
+		
+	}
 	
-					
-					/* S'il n'y as pas d'enfant (impossible de parcourir)
-					 * C'est une feuille
-					 * => Marquer la feuille
-					 * 
-					 * 
-					 * Sinon
-					 * 
-					 * 
-					  Enregistrer ses tableaux dans une autre variable
-						 * Ses tableaux sont applatie
-						 * Exemple: {objet,tableau1,tableau2}
-						 * tableau1 = [sous_element1,sous_element2]
-						 * tableau2 = [sous_element1]
-						 * (ne pas faire) tableau != tableau1 + tableau2
-						 * (mais faire ca)tableau == [sous_element1,sous_element2]
-					*/
-					 
+	
+	// Si l'element est un objet (contient un noeud / feature)
+	if (sommet.constructor.name == "Object"){
+		Object.assign(sommet,profondeur);
+		
+		//console.log(sommet,profondeur);
+		// Enregistrer profondeur.profondeur
+		//profondeur.hauteur += 1;
+		/*
+		console.log(profondeur.profondeur, profondeur.hauteur, sommet);
+		try {
+			essai_tmp[profondeur.profondeur][profondeur.hauteur] = sommet;
+		} 
+		
+		catch(error) {
+			essai_tmp[profondeur.profondeur] = ["profondeur: "+profondeur.profondeur];
+			essai_tmp[profondeur.profondeur][profondeur.hauteur] = sommet;
+			//console.log(error);
+		}*/
+
+
+		//essai_tmp[profondeur.profondeur][profondeur.hauteur] = sommet;
+		
+		//let profondeur = {profondeur : profondeur.profondeur};
+		
+		// Enregistrer profondeur.hauteur
+		
+			// Recuperer toutes les valeurs de l'objets
+			let values = Object.values(sommet);
+			
+			// Recuperer toutes les valeurs de type tableau de cet objet
+			let values_noeud = values.filter(a => a.constructor.name == "Array")
+			
+			
+			// Si la longueur de values_noeud > 0 on renvoi vrai sinon on ajoute 1 à feuille
+			bool_noeud = (values_noeud.length > 0 ? true : false);
+			//console.log(values_noeud);
+			// Si vrai
+			//console.log(sommet);
+			if (values_noeud.length > 0){
+				let tmp_array = [];
+				console.log("liste");
+				parent_noeud.push(sommet);
+				//console.log(parent_noeud);
+				values_noeud.forEach(function(v){
+					tmp_array = tmp_array.concat(v);
+				});
+
+				parcourire_arbre(tmp_array,profondeur,parent_noeud);
+				
+				
+			} else {
+				profondeur.feuilles_noeud = 1;
+			}
+			
+
+		}
+		
 
 	
 	
