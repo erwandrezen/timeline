a2e86c268c5f8e677abe17df777f7e064b7ab129
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-23 16:42:48 +0100

    sauvegarde

---
+++ html/hierarchypart/js/cacher_rect.js
@@ -0,0 +1,11 @@
+
+	/*
+	 * FONCTION			supprimer rectangles
+	*/
+	let cacher_branche = [];
+	let cacher_niveau = [];
+	let cacher_parent = 1;
+	function del_rect(){
+		// ajouter element dans cacher
+		// supprimer element dans cacher
+	}
