6539d637ec5d78ace491533b90abeef29c37df31
BLN <beranger.le.nautout@chu-rennes.fr>
2019-01-31 11:04:02 +0100

    essai de transition
    Essai de replacement de rectangle

---
+++ html/hierarchypart/transition/js.js
@@ -0,0 +1,74 @@
+//comparaison
+function get_position(){
+	let y_height = d3.select("#other")
+	.node()
+	.getBBox().height;
+	let y = d3.select("#rect")
+	.node()
+	.getBBox().y;
+	
+	let y_other = d3.select("#other")
+	.node()
+	.getBBox().y;
+
+	let y_move = y+y_height;
+	
+	return [y_move,y_other];
+}
+
+function bool_essai(){
+	let position = get_position();
+	if (position[0] > position[1]){
+		return true;
+	} else {
+		return false;
+	}
+	
+}
+
+//Fonction principal
+function move(){
+	
+	transition_50();
+	
+}
+
+//Button couleur
+function transtion_color(){
+	console.log("color");
+	d3.select("#rect")
+	.transition()
+	.style("fill","#FFEB3B");
+}
+
+//Button y=0
+function transition_0(){
+	d3.select("#rect")
+	.transition()
+	.attr("y","0");
+}
+
+//Button y=50
+function transition_50(){
+	let y = d3.select("#rect")
+	.node()
+	.getBBox().y;
+	
+	let rect = d3.select("#rect");
+	rect
+	.attr("y",y);
+	
+	rect
+	.transition()
+	.duration(1000)
+	.attr("y","56")
+	.each('end',  function(){
+		console.log(d,i);
+		let le_bool = bool_essai();
+		if (le_bool){
+			transtion_color();
+		}
+		 });
+
+	
+}
