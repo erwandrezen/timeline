770fb8722ee3010d67ebf7dbae8c95246116c3e3
BLN <beranger.le.nautout@chu-rennes.fr>
2019-02-07 17:16:05 +0100

    sauvegarde du main js

--- html/hierarchypart/js/main.js
+++ html/hierarchypart/js/main.js
@@ -1,45 +1,15 @@
-//Ouvrire le json
-//Parcourire en profondeur
-	//w,h referencer
-	//Ecrire les feuilles
-	//Ecrire x,y
-	//Referencer un ordre en fonction du parent
-
-
-//Ouvrire le json
-
-
-
 let json = "mocks/mock22.json";
+
 d3.json(json).then(function(data){
 	//console.log(data);
 	let width = 500;
-	//root = data['ownerlist'][0]['nodelist'];
-	root = data['ownerlist'][0]['nodelist'];
-
-	parcourir(root,width,15);
-
-	//console.log("*p",p)
-	//nombre max de profondeur
-
-	
 
 	let svg = d3.select("#hierarchypart").append("svg").attr("width",width)
 	.append("g");
 	
-	svg
-	.append("rect")
-	.attr("x","0")
-	.attr("y","0")
-	.style("width", "0px")
-	.style("height", "0px");
-	
-	svg
-	.append("polygon");
-	
-	//let datas = get_all_childrens(root);
-	resize(root)
-	//drawing("hierarchy", root);
+	root = data['ownerlist'][0]['nodelist'];
+	update();
+
 	
 	
 	
