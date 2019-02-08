4d398febe24d82f9e94354e63277e2d99169b793
BLN <beranger.le.nautout@chu-rennes.fr>
2019-02-08 16:44:53 +0100

    Ouverture fichier .json sans server
    Il y'avais un problème lors du chargement d'un fichier json sans serveur

--- html/hierarchypart/js/main.js
+++ html/hierarchypart/js/main.js
@@ -1,7 +1,7 @@
 let json = "mocks/mock22.json";
 
-d3.json(json).then(function(data){
-	//console.log(data);
+
+function hierarchy(data){
 	let width = 500;
 	
 	let hierarchypart = d3.select("#hierarchypart");
@@ -24,20 +24,48 @@ d3.json(json).then(function(data){
 	
 	root = data['ownerlist'][0]['nodelist'];
 	update();
+}
+
+
+
+function readMultipleFiles(evt) {
+    //Retrieve all the files from the FileList object
 
 	
-	
-	
-	
-	
-
-});
-
-/*
-var depthCount = function (branch,children) {
-    if (!branch[children]) {
-        return 1;
+    var files = evt.target.files;
+    if (files) {
+        for (var i = 0, f; f = files[i]; i++) {
+        	
+            var r = new FileReader();
+            
+            r.onload = (function (f) {
+            	
+                return function (e) {
+         
+                    let contents = e.target.result;
+                    
+                    data = JSON.parse(contents);
+                    hierarchy(data);
+                };
+            })(f);
+            
+           
+            
+            //f= file return
+            r.readAsText(f);
+            
+           
+            
+            
+        }
+        
+       
+        
+    } else {
+        alert("Failed to load files");
     }
-    return 1 + d3.max(branch[children].map(depthCount));
- }*/
+}
+
+//readMultipleFiles(json);
+document.getElementById('fileinput').addEventListener('change', readMultipleFiles, false);
 
